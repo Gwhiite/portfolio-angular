de0b5f39147d8b5992a020b61cba0a6b52f3cda0
gwhiite <guibrancodecampos@gmail.com>
2024-07-24 05:26:44 -0300

    feat: initial structure

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CardLinkComponent } from '../../components/card-link/card-link.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CardLinkComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {}
